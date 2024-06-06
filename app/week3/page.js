0951daca900e2842706667e618990c705f39272e
navrajveer <141467854+navrajveer@users.noreply.github.com>
2024-06-05 22:28:48 -0600

    Add week3 folder

---
+++ app/week3/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemList from './item-list';
+
+const Page = () => {
+  return (
+    <main className="max-w-3xl mx-auto p-6">
+      <h1 className="text-3xl font-semibold mb-4">Shopping List</h1>
+      <ItemList />
+    </main>
+  );
+};
+
+export default Page;
