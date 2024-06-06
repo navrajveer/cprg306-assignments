85317c818b5b5e5de95d7fbc5224ff1aa20aee2a
navrajveer <141467854+navrajveer@users.noreply.github.com>
2024-06-05 20:07:22 -0600

    Initial commit

---
+++ cprg306/app/page.js
@@ -0,0 +1,13 @@
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Navrajveer Kaur Mauhar</p>
+      <Link href="./week3">
+        Week 3 Assignment
+      </Link>
+    </div>
+  );
+}
---
+++ cprg306/app/week3/item.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Item = ({ name, quantity, category }) => {
+  return (
+    <li className="bg-gray-100 p-4 rounded-md shadow-md mb-4">
+      <div className="flex items-center justify-between">
+        <div>
+          <h2 className="text-lg font-semibold">{name}</h2>
+          <p className="text-gray-600">Quantity: {quantity}</p>
+        </div>
+        <span className="text-gray-600">{category}</span>
+      </div>
+    </li>
+  );
+};
+
+export default Item;
