2d2bc6b319d4c0ebc993cc68c70491fee96dd605
navrajveer <141467854+navrajveer@users.noreply.github.com>
2024-06-05 20:31:20 -0600

    Update student-info.js

---
+++ app/week2/student-info.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Navrajveer Kaur Mauhar</p>
+      <Link href="/week2">
+        github
+      </Link>
+    </div>
+  );
+}
